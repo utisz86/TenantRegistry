ab0eb7bc472274c565e2c8f76de75e0f80938db3
utisz86 <soma.kormanik@outlook.com>
2020-05-13 22:36:52 +0300

    Basic stage 0.0

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-from flask import Flask
+# Import modulst
+from flask import Flask, render_template, request, render_template
+
+
+# Establish flash
 app = Flask(__name__)
 
+# Scelton of the page
+
+# Main index page
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template(
+        "home.html"
+    )
+
+# About page
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+
+# Contact page
+@app.route(("/contact/"))
+def contact():
+    return render_template("contact.html")
+
+
